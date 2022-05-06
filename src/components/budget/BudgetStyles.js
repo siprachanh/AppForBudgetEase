32dfbe45c89a3031565fba9bff480af0d36fb390
Siprachanh <sipra.chant@gmail.com>
2022-05-06 16:17:46 -0500

    added minimal pie chart on budget page

--- src/components/budget/BudgetStyles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core";
-export const MakeStyles = () => ({
-    incomeTotal: {
-        borderBottom: '10px solid rgba(0, 255, 0, 0.5)', 
-    },
-    expenseTotal: {
-        borderBottom: '10px solid rgba(0, 255, 0, 0.5)', 
-    }
-});
